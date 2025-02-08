11bca4421a9ee5f16145959c7a1b6b4fb3c0fb38
umayanga amith <umayangaamith2@gmail.com>
2025-02-08 16:50:07 +0000

    jason route

--- myApp.js
+++ myApp.js
@@ -1,45 +1,26 @@
 
+
+require('dotenv').config(); // Load environment variables from .env file
 let express = require('express');
 let app = express();
 
-app.get("/", function (req, res){
+// Serve the index.html file
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+// Serve static files from the /public directory
+app.use("/public", express.static(__dirname + "/public"));
 
+// Handle the /json route
 
+app.get("/json", (req, res) => {
+    let message = "Hello json"
+  if ( process.env.MESSAGE_STYLE==='uppercase') {
+      message = message.toUpperCase();
+  }
+  res.json({message})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// Export the app for testing purposes
+module.exports = app;
